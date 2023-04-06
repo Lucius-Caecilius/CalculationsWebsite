be63dbc3f01a0506542c51b189c1a92d275d99d7
Lucius-Caecilius <afsavory@outlook.com>
2023-04-06 15:32:55 +0100

    General updates and neatening the code

--- subpages/maths/script.js
+++ subpages/maths/script.js
@@ -2,14 +2,11 @@ function input_from_user(a, b, c){
     var a_term = parseFloat(document.getElementById(a).value);
     var b_term = parseFloat(document.getElementById(b).value);
     var c_term = parseFloat(document.getElementById(c).value);
-    console.log(a_term);
-    console.log(b_term);  
-    console.log(c_term);
-    temp1 = (b_term*b_term) - (4*a_term*c_term);
-    temp2 = (-1*b_term) - Math.sqrt(temp1);
-    temp3 = (-1*b_term) + Math.sqrt(temp1);
-    temp4 = temp2/(2*a_term);
-    temp5 = temp3/(2*a_term);
-    document.getElementById("x_value_a").innerHTML = temp4
-    document.getElementById("x_value_b").innerHTML = temp5
+    inside_sqrt = (b_term*b_term) - (4*a_term*c_term);
+    negative_possibility = (-1*b_term) - Math.sqrt(inside_sqrt);
+    positive_possibility = (-1*b_term) + Math.sqrt(inside_sqrt);
+    negative_possibility = negative_possibility/(2*a_term);
+    positive_possibility = positive_possibility/(2*a_term);
+    document.getElementById("x_value_a").innerHTML = "The x value is: " + negative_possibility
+    document.getElementById("x_value_b").innerHTML = "Or the x value is: " + positive_possibility
 }
