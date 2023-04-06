2ec7fa110a28ec4d440f98c90001edffb76b28c9
Lucius-Caecilius <afsavory@outlook.com>
2023-04-06 19:37:56 +0100

    Adding equations and functionality to physics.html

--- subpages/physics/script.js
+++ subpages/physics/script.js
@@ -17,3 +17,28 @@ function filterContent() {
       }
     }
 }
+
+function standard_equation(t, b_1, b_2, id){
+    var top, base_1, base_2;
+    top = document.getElementById(t).value;
+    base_1 = document.getElementById(b_1).value;
+    base_2 = document.getElementById(b_2).value;
+    console.log(top);
+    console.log(base_1);
+    console.log(base_2);
+    if(top == ""){
+        value_to_return = base_1*base_2;
+        console.log(value_to_return);
+        document.getElementById(id).innerHTML = "The unknown variable is: " + value_to_return;
+    } 
+    else if(base_1 == ""){
+        value_to_return = top/base_2;
+        console.log(value_to_return);
+        document.getElementById(id).innerHTML = "The unknown variable is: " + value_to_return;
+    } 
+    else{
+        value_to_return = top/base_1;
+        console.log(value_to_return);
+        document.getElementById(id).innerHTML = "The unknown variable is: " + value_to_return;
+    }
+}
