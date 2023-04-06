bfa2879cfa1edd16ba8df35c8d4e357938f93995
Lucius-Caecilius <afsavory@outlook.com>
2023-04-06 16:14:16 +0100

    Adding functionality to pythagoras

--- subpages/maths/script.js
+++ subpages/maths/script.js
@@ -1,4 +1,24 @@
-function input_from_user(a, b, c){
+function pythagoras(a, b, hyp){
+    var side_1 = parseFloat(document.getElementById(a).value);
+    var side_2 = parseFloat(document.getElementById(b).value);
+    var hypotenuse_status = document.getElementById(hyp).checked;
+    if(hypotenuse_status){
+        hyp_sqrd = Math.pow(side_1, 2) + Math.pow(side_2, 2)
+        hypotenuse = Math.sqrt(hyp_sqrd)
+        document.getElementById("unknown_side").innerHTML = "The hypotenuse is: " + hypotenuse
+    } else{
+        if(side_1 > side_2){
+            unknown_side_sqrd = Math.pow(side_1, 2) - Math.pow(side_2, 2)
+            unknown_side = Math.sqrt(unknown_side_sqrd)
+        } else{
+            unknown_side_sqrd = Math.pow(side_2, 2) - Math.pow(side_1, 2)
+            unknown_side = Math.sqrt(unknown_side_sqrd)
+        }
+        document.getElementById("unknown_side").innerHTML = "The unknown side is: " + unknown_side
+    }
+}
+
+function quadratic_formula(a, b, c){
     var a_term = parseFloat(document.getElementById(a).value);
     var b_term = parseFloat(document.getElementById(b).value);
     var c_term = parseFloat(document.getElementById(c).value);
