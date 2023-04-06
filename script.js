ded2bf9822ac9df80ff0be0b43dac05c6160e529
Lucius-Caecilius <afsavory@outlook.com>
2023-04-06 17:42:43 +0100

    coding (read: copying) dropdown for search

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 console.log("Hello World")
-
 function myFunction(){
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("mySearch");
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, a, i;
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("menu");
-    li = ul.getElementsByTagName("li");
-    for (i=0; i <li.length; i++){
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1){
-            li[i].style.display = "";
-        } else{
-            li[i].style.display = "none";
-        }
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
     }
 }
